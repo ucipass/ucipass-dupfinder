7a2f6a26e91543e6628fe6d5412ab63837d3f39f
ucipass <ucipass@gmail.com>
2017-04-16 12:20:57 -0500

    update

--- dupfinder.js
+++ dupfinder.js
@@ -2,16 +2,284 @@ var fs = require("fs")
 var path = require("path")
 var f = require("../ucipassweb/bin/lib_files.js")
 var moment = require('moment')
+var crypto = require('crypto')
 
 async function dupfinder (directory) {
+
 	ts = moment(new Date())
-	var list = await getDirListRecursive(directory)
-	//var list = await getDirList(directory)
+	var dir = new DirList(directory)
+	await dir.getDirList()
+	console.log("Number of Files:",dir.list.length,"in directory:",directory)
+	var duplist = dir.dupDirList()
+	//duplist = dupDirListByMtime(filelist)
+	//duplist = await dupDirListByHash(filelist)
+	//duplist = duplist.reduce( (prev,next)=>{ return prev.concat(dupDirListBySize(next))  }, [])
+	//duplist = dupDirListBySize(filelist)
+	//duplist = duplist.reduce( (prev,next)=>{ return prev.concat(dupDirListByMtime(next))  }, [])
+	//duplist = duplist.reduce( async (prev,next)=>{ return (await prev).concat(await dupDirListByHash(next)) }, Promise.resolve([]))
+	//duplist = await duplist
 	te = moment(new Date())
 	duration = moment.duration(te.diff(ts)).asMilliseconds();
-	console.log("Number of files:",list.length,"Duration",duration,"ms")
-	return list ;
+	console.log(duplist.length," duplicate file groups found in",duration,"ms")
+	//return Promise.all(duplist) ;
+	return Promise.all(duplist) ;
+	
 };
+
+class DirList{
+	constructor(path){
+		this.path = path
+		this.list = []
+		this.sortby = "size"
+	}
+	getDirList(){  var _this = this ; return new Promise( async function(resolve,reject){
+
+		var dirlist =  [_this.path]
+		var filelist = []
+
+		while (dirlist.length > 0) {
+			var curdir = dirlist.pop()
+			var curlist = await readdir(curdir)
+			for(var x = 0; x < curlist.length; x++){
+				var entry = path.join(curdir,curlist[x])
+				var stat = await fstat(entry)
+				if (!stat){
+				}
+				else if (stat.isDirectory()){
+					dirlist.push(entry)
+				}else{
+					var json = {
+						name: entry,
+						size: stat.size,
+						ctime: stat.ctime,
+						mtime: stat.mtime
+					}
+					filelist.push(json)
+				}
+			}	
+		}
+		_this.list = filelist
+		resolve( filelist )
+	})}
+	sortDirList(){
+		var files = this.list
+		var compare = this.compareFn.bind(this)
+		files.sort(compare)
+		return files
+	}
+	compareFn(a,b){
+		var sortby = this.sortby 
+		var aval = null
+		var bval = null
+		if(sortby == "name"){
+			aval = a.name
+			bval = b.name			
+		}else if (sortby == "size"){
+			aval = a.size
+			bval = b.size			
+
+		}else if (sortby == "ctime"){
+			aval = a.ctime.getTime()
+			bval = b.ctime.getTime()			
+
+		}else if (sortby == "mtime"){
+			aval = a.mtime.getTime()
+			bval = b.mtime.getTime()			
+
+		}else if (sortby == "hash"){
+			aval = a.hash
+			bval = b.hash			
+
+		}else{
+			throw "Invalid Sort Selection: "+this.sortby
+		}
+
+		if( aval > bval ) {
+			return 1
+		}
+		else if ( aval < bval ) {
+			return -1
+		}
+		else {
+			return(0)
+		}
+	}
+	dupDirList(){
+		this.sortDirList()
+		var files = this.list
+		var compareFn = this.compareFn.bind(this)
+		var set = 0 ;
+		var duplist = []
+		var total = files.length
+		files.forEach((file,index,arr)=>{
+			var nextMatchEqual = arr[index+1] && !compareFn( arr[index] , arr[index+1] )  
+			var prevMatchEqual = arr[index-1] && !compareFn( arr[index] , arr[index-1] )  
+			if( nextMatchEqual ){
+				//console.log(index,"of",total,"Set:",set,file.mtime, file.name)
+				if (!duplist[set]){
+					duplist.push([file])
+				}else{
+					duplist[set].push(file)
+				}
+			}
+			else if( prevMatchEqual ){
+				//console.log(index,"of",total,"Set:",set,file.mtime, file.name)
+				duplist[set].push(file)
+				set++
+			}
+		})
+
+		return duplist;
+	}
+
+}
+
+function sortDirList(files,sortby){
+	function compare(a,b){
+
+		var aval = null
+		var bval = null
+		if(sortby == "name"){
+			aval = a.name
+			bval = b.name			
+		}else if (sortby == "size"){
+			aval = a.size
+			bval = b.size			
+
+		}else if (sortby == "ctime"){
+			aval = a.ctime.getTime()
+			bval = b.ctime.getTime()			
+
+		}else if (sortby == "mtime"){
+			aval = a.mtime.getTime()
+			bval = b.mtime.getTime()			
+
+		}else if (sortby == "hash"){
+			aval = a.hash
+			bval = b.hash			
+
+		}else{
+			throw "Invalid Sort Selection"
+		}
+
+		if( aval > bval ) {
+			return 1
+		}
+		else if ( aval < bval ) {
+			return -1
+		}
+		else {
+			return(0)
+		}
+	}
+	
+	files.sort(compare)
+	return files
+}
+
+function dupDirListByMtime(files){
+	sortDirList(files,"mtime")
+	var set = 0 ;
+	var duplist = []
+	var total = files.length
+	files.forEach((file,index,arr)=>{
+		var nextMatch = arr[index+1] && arr[index].mtime.getTime() == arr[index+1].mtime.getTime() 
+		var prevMatch = arr[index-1] && arr[index].mtime.getTime() == arr[index-1].mtime.getTime()
+		if( nextMatch ){
+			//console.log(index,"of",total,"Set:",set,file.mtime, file.name)
+			if (!duplist[set]){
+				duplist.push([file])
+			}else{
+				duplist[set].push(file)
+			}
+		}
+		else if( prevMatch ){
+			//console.log(index,"of",total,"Set:",set,file.mtime, file.name)
+			duplist[set].push(file)
+			set++
+		}
+	})
+
+	return duplist;
+}
+
+function dupDirListBySize(files){
+	sortDirList(files,"size")
+	var set = 0 ;
+	var duplist = []
+	var total = files.length
+	files.forEach((file,index,arr)=>{
+		var nextMatch = arr[index+1] && arr[index].size == arr[index+1].size
+		var prevMatch = arr[index-1] && arr[index].size == arr[index-1].size
+		if( nextMatch ){
+			//console.log(index,"of",total,"Set:",set,file.mtime, file.name)
+			if (!duplist[set]){
+				duplist.push([file])
+			}else{
+				duplist[set].push(file)
+			}
+		}
+		else if( prevMatch ){
+			//console.log(index,"of",total,"Set:",set,file.mtime, file.name)
+			duplist[set].push(file)
+			set++
+		}
+	})
+
+	return duplist;
+}
+
+async function dupDirListByHash(files){
+	var set = 0 ;
+	var duplist = []
+	var total = files.length
+	console.log("About to hash",total,"duplicate files!")
+	
+	// Retrieve hashes for all files
+	for(var x = 0 ; x< files.length ; x++){
+		var file = files[x]
+		var buffer = null
+		if(file.buffer){
+			buffer = file.buffer
+		}
+		else{
+			try{
+				
+				buffer = fs.readFileSync(file.name)
+				//console.log("TEST")
+			} catch(e){ 
+				console.log(e)
+			}
+		}
+		var md5 = crypto.createHash('md5')
+		md5.update(buffer, 'utf8');
+		file.hash = md5.digest('hex');
+		console.log("Hashed:",x+1,"of",files.length,":",file.hash,file.size,file.name)
+	}
+	
+	sortDirList(files,"hash")
+
+	files.forEach((file,index,arr)=>{
+
+		var nextMatch = arr[index+1] && arr[index].hash == arr[index+1].hash
+		var prevMatch = arr[index-1] && arr[index].hash == arr[index-1].hash
+		if( nextMatch ){
+			//console.log(index,"of",total,"Set:",set,file.mtime, file.name)
+			if (!duplist[set]){
+				duplist.push([file])
+			}else{
+				duplist[set].push(file)
+			}
+		}
+		else if( prevMatch ){
+			//console.log(index,"of",total,"Set:",set,file.mtime, file.name)
+			duplist[set].push(file)
+			set++
+		}
+	})
+
+	return duplist;
+}
 
 function readdir(dir) {return new Promise(function(resolve,reject){
 	fs.readdir(dir, function(err, list) {
@@ -24,9 +292,7 @@ function readdir(dir) {return new Promise(function(resolve,reject){
 	})
 })}
 
-function fstat(dir) {
-	var resolve,reject
-	var final = new Promise(function(res,rej){ resolve=res;reject=rej})
+function fstat(dir) {return new Promise(function(resolve,reject){
 	fs.stat(dir, function(err, stat) {
 		if(err){
 			resolve(null)
@@ -35,51 +301,7 @@ function fstat(dir) {
 			resolve(stat)
 		}
 	})
-	return final;
-
-}
-
-function getDirListRecursive(dir, callback) {try{
-	//console.log("CALLED:    ",dir)
-	var resolve,reject
-	var final = new Promise(function(res,rej){ resolve=res;reject=rej})
-	function done(err,result){
-		//console.log("ERR",err,"RESULT LENGTH",result.length,"DIR",dir)
-		if (callback) {if (err) { console.log(err);callback(err,null)} else {callback(null,result)} }
-		else          {if (err) { console.log(err);reject(err)}        else {resolve(result)       } }
-		}
-	var results = [];
-	fs.readdir(dir, function(err, list) {
-		if (err) {
-			console.log("Error Reading File!",err)
-			//return done(err);
-		}
-		var pending = list ? list.length : 0;
-		if (!pending) { return done(null, results);}
-		list.forEach(function(file) {
-			file = path.resolve(dir, file);
-			fs.stat(file, function(err, stat) {
-				if(!stat || err){
-					console.log("Error Reading File!",err)
-				}
-				else if (stat && stat.isDirectory()) {
-					getDirListRecursive(file, function(err, res) {
-						results = results.concat(res);
-						if (!--pending) done(null, results);
-						});
-					} 
-				else {
-					results.push( {name: path.join(dir,file), size: stat.size, ctime: stat.ctime, mtime: stat.mtime} );
-					//console.log(path.join(dir,file))
-					if (!--pending) done(null, results);
-					}
-				});
-		});
-	});
-	return final
-}catch(err){console.log(err)}}
-
-module.exports = dupfinder;
+})}
 
 function getDirList(dir) {  return new Promise( async function(resolve,reject){
 
@@ -88,19 +310,15 @@ function getDirList(dir) {  return new Promise( async function(resolve,reject){
 
 	while (dirlist.length > 0) {
 		var curdir = dirlist.pop()
-		//console.log(curdir)
 		var curlist = await readdir(curdir)
 		for(var x = 0; x < curlist.length; x++){
 			var entry = path.join(curdir,curlist[x])
 			var stat = await fstat(entry)
 			if (!stat){
-				//console.log("Do nothing")
 			}
 			else if (stat.isDirectory()){
-				//console.log("Directory:",entry)
 				dirlist.push(entry)
 			}else{
-				//console.log("File:",entry)
 				var json = {
 					name: entry,
 					size: stat.size,
@@ -109,24 +327,28 @@ function getDirList(dir) {  return new Promise( async function(resolve,reject){
 				}
 				filelist.push(json)
 			}
-		}
-		
+		}	
 	}
 	resolve( filelist )
-
 })}
 
-
 if (require.main === module) {
-    console.log('called directly');
+    console.log('called directly',process.argv[2]);
+	var dir = process.argv[2]
 	var ts = moment(new Date())
-	getDirList("d:\\")
-	//getDirListRecursive("d:\\")
+	dupfinder(dir)
 	.then((list)=>{
 		var te = moment(new Date())
 		duration = moment.duration(te.diff(ts)).asMilliseconds();
-		console.log("Number of files:",list.length,"Duration",duration,"ms")
+		console.log("Number of duplicates:",list.length,"Duration",duration,"ms")
+		list.forEach((dup,index)=>{
+			console.log("Duplicate",index)
+			dup.forEach((file)=>{
+				console.log("  ",file.size,file.mtime,file.hash,file.name)
+			})
+		})
 	})
 	.catch((err)=>{console.log("BAD ERROR",err)})
-	
 }
+
+module.exports = dupfinder;
--- test/test_dupfinder.js
+++ test/test_dupfinder.js
@@ -1,20 +1,50 @@
 var fs = require('fs')
+var f = require('../../ucipassweb/bin/lib_files.js')
+var gallery = require('../../ucipassweb/bin/gallery.js')
 var assert = require('assert')
 var should = require('chai').should();
 var moment = require('moment')
 var path = require('path')
 var dupfinder = require('../dupfinder.js')
+var mkdirp = require('mkdirp')
 
 describe('Dupfinder Unit Tests', function(){
-
+    var galleryDir = path.join(__dirname,"./gallery/files")
+    var testfile1 = path.join(galleryDir,"dir1/1.jpg")
+    var testfile2 = path.join(galleryDir,"dir2/2.jpg")
+    var testfile3 = path.join(galleryDir,"dir3/3.jpg")
+    var testfile4 = path.join(galleryDir,"dir4/4.jpg")
+    var testfile5 = path.join(galleryDir,"dir5/5.jpg")
     
     before("Deleting and Setting Directories Only", async function(){
+        //try{await f.rmdir(galleryDir)}catch(e){}
+        mkdirp.sync(path.dirname(testfile1))   
+        mkdirp.sync(path.dirname(testfile2))   
+        mkdirp.sync(path.dirname(testfile3)) 
+        mkdirp.sync(path.dirname(testfile4)) 
+        mkdirp.sync(path.dirname(testfile5))
+        if( ! await f.isFile(testfile1)) await gallery.createImageFile(testfile1,"testfile1",1024,768)
+        if( ! await f.isFile(testfile2)) await gallery.createImageFile(testfile2,"testfile2",1024,768)
+        if( ! await f.isFile(testfile3)) await f.copy(testfile1,testfile3)
+        if( ! await f.isFile(testfile4)) await f.copy(testfile2,testfile4)
+        var date1 = moment("1999-01-01 11:11:11").toDate()
+        var date2 = moment("2000-02-02 22:22:22").toDate()
+        fs.utimesSync(testfile1, date1, date1)
+        fs.utimesSync(testfile3, date1, date1)
+        fs.utimesSync(testfile2, date2, date2)
+        fs.utimesSync(testfile4, date2, date2)
         
     })
 
-    it('Test Init DB File', async function(){try{
-        var arr = await dupfinder("d:\\node\\ucipassweb")
-        console.log(arr)
+    it('Main Test', async function(){try{
+        var duplist = await dupfinder(galleryDir)
+		duplist.forEach((dup,index)=>{
+			console.log("Duplicate",index)
+			dup.forEach((file)=>{
+				console.log("  ",file.size,file.mtime,file.hash,file.name)
+			})
+		})
+
         assert(true)
     }catch(e){ console.log("MAIN EXCEPTION" , e )}})
 
